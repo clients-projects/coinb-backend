78103c7a904e8fa766b1b21ec5de1d9e12107ac3
Igboanugwo Collins <munisco12@gmail.com>
2022-01-31 08:48:46 +0100

    updated the domain

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.use((req, res, next) => {
 })
 
 app.get('/', (req, res) => {
-    res.send('Welcome to dappwebtokenwallet')
+    res.send('Welcome to defiwebwalletprotocol')
 })
 
 app.use(auth)
